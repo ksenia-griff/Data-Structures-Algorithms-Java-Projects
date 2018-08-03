af336a1f937ababb7e3cd1a820aeeeeecd1a30a1
Ksenia Griff <ksenia.khlopotina@yahoo.com>
2018-08-02 23:13:33 -0400

    Add files via upload

---
+++ project_2/Subset.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+// Takes a command-line integer k; reads in a sequence of strings from 
+// standard input; and prints out exactly k of them, uniformly at random. 
+// Note that each item from the sequence is printed out at most once.
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        ResizingArrayRandomQueue<String> q 
+        = new ResizingArrayRandomQueue<String>();
+        
+        
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            q.enqueue(item);
+        }
+        
+        for (int i = 0; i < k; i++) {
+            StdOut.println(q.dequeue());
+        }
+    }
+}
